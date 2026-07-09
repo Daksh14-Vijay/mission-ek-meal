31eaf5719ffd5a48466ba935df1abe75de71c153
Daksh14-Vijay <dakshvj03@gmail.com>
2026-07-09 11:47:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,84 +1,15 @@
-// Navbar Shadow on Scroll
+function openQR(){
+document.getElementById("qrModal").style.display="block";
+}
 
-window.addEventListener("scroll", () => {
+function closeQR(){
+document.getElementById("qrModal").style.display="none";
+}
 
-    const navbar = document.querySelector(".navbar");
+window.onclick=function(event){
+const modal=document.getElementById("qrModal");
 
-    if (window.scrollY > 50) {
-        navbar.style.boxShadow = "0 10px 30px rgba(0,0,0,0.1)";
-    } else {
-        navbar.style.boxShadow = "none";
-    }
-
-});
-
-
-// Fade In Animation
-
-const observer = new IntersectionObserver((entries) => {
-
-    entries.forEach(entry => {
-
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-
-    });
-
-}, {
-    threshold: 0.2
-});
-
-document.querySelectorAll(".about, .card, .join")
-.forEach((el) => {
-    el.classList.add("hidden");
-    observer.observe(el);
-});
-
-
-// Hero Text Animation
-
-window.addEventListener("load", () => {
-
-    const hero = document.querySelector(".hero-content");
-
-    hero.style.opacity = "0";
-    hero.style.transform = "translateY(40px)";
-
-    setTimeout(() => {
-
-        hero.style.transition = "1s ease";
-        hero.style.opacity = "1";
-        hero.style.transform = "translateY(0px)";
-
-    }, 300);
-
-});
-
-
-// Button Hover Sound Effect (Optional)
-
-document.querySelectorAll(".btn").forEach(btn => {
-
-    btn.addEventListener("mouseenter", () => {
-
-        btn.style.transform = "translateY(-5px) scale(1.03)";
-
-    });
-
-    btn.addEventListener("mouseleave", () => {
-
-        btn.style.transform = "translateY(0px) scale(1)";
-
-    });
-
-});
-
-
-// Smooth Reveal Cards
-
-document.querySelectorAll(".card").forEach((card, index) => {
-
-    card.style.transitionDelay = `${index * 0.2}s`;
-
-});
+if(event.target==modal){
+modal.style.display="none";
+}
+}
